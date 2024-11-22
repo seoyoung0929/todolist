1a34af26018658ba95c48ccc6dad8df1c04d2965
seoyoung0929 <csyoung0929@gmail.com>
2024-11-22 15:38:36 +0900

    todo-useEffect

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,30 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoHd from './TodoHd';
 import TodoEditor from './TodoEditor';
 import TodoList from './TodoList';
-import { mockTodoData } from '@/data/todoData';
 
 const Todo = () => {
   // 할일 목록을 업데이트 해줘야한다
-  const [todos, setTodos] = useState(mockTodoData);
+  const [todos, setTodos] = useState([]);
+
+  // 마운트 시
+  // 1. 초기 데이터 로드
+  useEffect(() => {
+    // localStorage에서 'todos' 키로 저장된 데이터를 가져옴
+    // JSON.parse() 함수를 이용하여 문자열을 객체로 변환
+    const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+    // 가져온 데이터로 상태 업데이트
+    setTodos(savedTodos);
+  }, []); // 빈 배열: 컴포넌트가 처음 마운트될 때만 실행
+
+  // 2. 데이터 자동 저장
+  useEffect(() => {
+    // todos 상태가 변경될 때마다 localStorage에 저장
+    // JSON.stringify() 함수를 이용하여 객체를 문자열로 변환
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]); // todos가 변경될 때마다 실행
 
   // 할일을 입력하면 추가하는 함수
   // 입력된 할일이 들어옴 - 매개변수로 받아주자
