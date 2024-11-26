06144164cda4b02f0137198d3b7b52eb7845e32a
seoyoung0929 <csyoung0929@gmail.com>
2024-11-26 14:32:28 +0900

    useContext

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,19 @@
 'use client';
 
+import { Provider } from '@/components/ui/provider';
 import './globals.css';
 import { Providers } from '@/contexts';
+import { ThemeProvider } from '@mui/material';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <Providers>{children}</Providers>
+        <ThemeProvider>
+          <Provider>
+            <Providers>{children}</Providers>
+          </Provider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,15 @@ import Image from 'next/image';
 import { useContext } from 'react';
 import { ThemaContext } from './layout';
 import { useTheme } from '@/contexts/ThemeContext';
+import { Button } from '@chakra-ui/react';
 
 export default function Home() {
   const theme = useTheme();
   return (
     <div className={theme.background}>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
       <Todo />
     </div>
   );
